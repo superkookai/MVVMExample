efbc0c331c4c560e9007e0ff2354fb59dd559391
superkookai <weerawut@live.com>
2025-03-03 14:57:48 +0700

    Initial Commit

---
+++ MVVMExample/MVVMExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMExampleApp.swift
+//  MVVMExample
+//
+//  Created by Weerawut Chaiyasomboon on 03/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct MVVMExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
